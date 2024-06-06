db1a50ce32108a7c1dc4774f3369dc068b5951ec
Nicollas R Deluca M <nicollas83@gmail.com>
2024-06-06 00:05:49 -0300

    Criação do services, component login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,18 @@
+import { SpotifyService } from './../../services/spotify.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.componet.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private spotifyService: SpotifyService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  abrirPaginaLogin() {
+    // window.location.href =
+    console.log(this.spotifyService.obterUrlLogin());
   }
-
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { RouterModule } from '@angular/router';
+import { LoginRotas } from './login.routes';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent],
+  imports: [CommonModule, RouterModule.forChild(LoginRotas)],
 })
-export class LoginModule { }
+export class LoginModule {}
